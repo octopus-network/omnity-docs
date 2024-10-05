219b356a4becfbfb4c6dbde083c57dfb9e50b44a
Ting A Lin <linshaoting6@gmail.com>
2024-10-05 08:06:31 +0700

    redirect homepage

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'OMNITY API',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://docusaurus.io',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,26 +3,9 @@ import clsx from 'clsx';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import styles from './index.module.css';
-
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header style={{textAlign:'center', position:'relative', top:'100px', color:'#3b08a0'}}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-      </div>
-    </header>
-  );
-}
+import { Redirect } from '@docusaurus/router';
 
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
-  return (
-    <Layout
-      title={`${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-    </Layout>
-  );
+  return <Redirect to="/docs/intro" />;
 }
